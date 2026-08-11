226ef680022971412670493dd39d39b5504b638c
DiengAda <adamadieng1957@gmail.com>
2026-08-12 01:04:01 +0200

    Ajout des pièces avec la même logique JS

--- app.py
+++ app.py
@@ -2,12 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-BILLETS = [100, 50, 20, 10, 5]
+BILLETS = [5, 10, 20, 50, 100, 200, 500]
+PIECES = [0.01, 0.02, 0.05, 0.10, 0.20, 0.50, 1, 2]
 
 @app.route("/")
 def index():
     # return "Hello, caisse!"
-    return render_template("index.html", billets=BILLETS)
+    return render_template("index.html", billets=BILLETS, pieces=PIECES)
 
 if __name__ == "__main__":
     app.run(debug=True)
