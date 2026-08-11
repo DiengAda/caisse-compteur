e72607f3fb31356b151a46d1f4858c473748afe0
DiengAda <adamadieng1957@gmail.com>
2026-08-11 17:23:59 +0200

    Génération des billets via boucle Jinja

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+BILLETS = [100, 50, 20, 10, 5]
+
 @app.route("/")
 def index():
     # return "Hello, caisse!"
-    return render_template("index.html")
+    return render_template("index.html", billets=BILLETS)
 
 if __name__ == "__main__":
     app.run(debug=True)
